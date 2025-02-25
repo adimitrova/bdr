448d784ef364e919c595d2f36ae7019f7955e4f0
adimitrova <a.dimitrovaa@gmail.com>
2025-02-25 20:28:29 +0100

    fix typo

--- src/gamer.py
+++ src/gamer.py
@@ -5,11 +5,10 @@ from src.type import Type
 
 class Gamer:
     def __init__(self, name: str, email_address: str, notifier: Notifier):
-        self.id: uuid.UUID = uuid4()
+        self.id = uuid4()
         self.name: str = name
         self.email_address: str = email_address
         self.notifier: Notifier = notifier
 
     def notify(self, market_id: str, type_obj: Type) -> None:
         self.notifier.notify(market_id=market_id, type_obj=type_obj, recipient=self)
-s
